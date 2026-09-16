36bc437e2b59eee1f03fd144a251eecf789a102b
Aysha Hasan <1220352@student.birzeit.edu>
2026-09-16 09:55:38 +0300

    encode and decode strings

---
+++ encodeDecodeStr.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        encoded = ""
+
+        for word in strs:
+            length = len(word)
+            encoded = encoded + str(length) + "#" + word
+
+        return encoded
+
+
+    def decode(self, s: str) -> List[str]:
+
+        result = []
+        i = 0
+
+        while i < len(s):
+
+            j = i
+
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i:j])
+
+            start = j + 1
+            end = start + length
+
+            word = s[start:end]
+
+            result.append(word)
+
+            i = end
+
+        return result
